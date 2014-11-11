f2caf9ecf0f0c1d70755f512d9fab5d22d9b4f62
Robert Vickerstaff <robert.vickerstaff@emr.ac.uk>
2014-11-11 13:49:03 +0000

    added next_line function to ftemplate

--- ftemplate.py
+++ ftemplate.py
@@ -10,24 +10,70 @@ import inspect,os,re,time,random,subprocess
 
 qsub_log_dir = '/home/vicker/qsub_logs'
 
+def next_line(fname,sep='guess',comment='#',skip_blank=True):
+    '''
+    return next line of file split by separator guessed from file extension
+    or given as option
+    
+    sep=None => do not split into tokens
+    sep='' => whitespace as separtor
+    '''
+
+    if type(fname) == str:
+        #treat fname as file name
+        f = open(fname)
+
+        if sep == 'guess':
+            #guess separator from filename extension
+            exts = {'.tsv':'\t', '.tab':'\t', '.csv':',', '.ssv':''}
+            ext = fname.split('.')[-1]
+            assert ext.lower() in exts.keys()
+            sep = exts[ext.lower()]
+            
+    else:
+        #treat fname as file handle
+        f = fname
+        assert sep != 'guess'
+
+    for line in f:
+        line = line.strip()
+        
+        #remove any comment
+        if comment != None:
+            if line.find(comment) != -1:
+                line = line[:line.index(comment)].strip()
+                
+        #skip blank lines
+        if line == '': continue
+        
+        #split using separator, yield token list
+        if sep != None:
+            tok = line.split(sep)
+            yield tok
+            
+        #yield whole line
+        yield line
+
+    f.close()
+
 def next_sample(fname):
-	'''
-	get next non commented sample name
-	skip blank lines
-	'''
-	
-	f = open(fname)
-	
-	for line in f:
-		if line == '': break
-		line = line.strip()
-		if line.find('#') != -1:
-			line = line[:line.index('#')].strip()
-		if line == '': continue
-		
-		yield line
-	
-	f.close()
+    '''
+    get next non commented sample name
+    skip blank lines
+    '''
+
+    f = open(fname)
+
+    for line in f:
+        if line == '': break
+        line = line.strip()
+        if line.find('#') != -1:
+            line = line[:line.index('#')].strip()
+        if line == '': continue
+        
+        yield line
+
+    f.close()
 
 def myqsub(inp,out=None,dic=None,sge=True):
     '''
@@ -73,84 +119,84 @@ def myqsub(inp,out=None,dic=None,sge=True):
     os.system('cp '+out+' '+qsub_log_dir+'/%d.sge'%jobnumber)
 
 def ftemplate(inp,out,dic=None,sge=True):
-	'''
-	inp - template file
-	out - output file
-	dic - key:value dictionary
-	
-	replace @{key} with value for all placeholders in the input file
-	write to output file
-	
-	if dic == None, get key value pairs from name space of calling function
-	
-	sge == True => fix commented shell variables that confuse sun grid engine
-	'''
+    '''
+    inp - template file
+    out - output file
+    dic - key:value dictionary
 
-	#get local variables from calling function
-	if dic == None: dic = get_locals(inspect.currentframe().f_back.f_locals)
-		
-	#read template, fill out place holders
-	f = open(inp)
-	data = sub_templates(f.read(),dic,sge)
-	f.close()
+    replace @{key} with value for all placeholders in the input file
+    write to output file
 
-	#write out final file
-	fout = open(out,'wb')
-	fout.write(data)
-	fout.close()
+    if dic == None, get key value pairs from name space of calling function
+
+    sge == True => fix commented shell variables that confuse sun grid engine
+    '''
+
+    #get local variables from calling function
+    if dic == None: dic = get_locals(inspect.currentframe().f_back.f_locals)
+        
+    #read template, fill out place holders
+    f = open(inp)
+    data = sub_templates(f.read(),dic,sge)
+    f.close()
+
+    #write out final file
+    fout = open(out,'wb')
+    fout.write(data)
+    fout.close()
 
 def get_locals(tvar):
-	'''
-	extract only simple local variables from the frame
-	'''
+    '''
+    extract only simple local variables from the frame
+    '''
 
-	dic = {}
-	allowed = [str,int,float,bool]
-	
-	for k,v in tvar.iteritems():
-		if type(k) != str: continue
-		if not type(v) in allowed: continue 
-		dic[k] = v
-		
-	del tvar
-	return dic
+    dic = {}
+    allowed = [str,int,float,bool]
+
+    for k,v in tvar.iteritems():
+        if type(k) != str: continue
+        if not type(v) in allowed: continue 
+        dic[k] = v
+        
+    del tvar
+    return dic
 
 def sub_templates(data,dic,sge=False):
-	'''
-	substitute values for place holders
-	'''
-	
-	#replace @{key} with value
-	for key in dic.iterkeys():
-		if not '@{%s}'%key in data: continue
-		data = data.replace('@{%s}'%key, str(dic[key]))
+    '''
+    substitute values for place holders
+    '''
 
-	#check all placeholders were replaced
-	if '@{' in data:
-		for m in re.finditer('@{.*}',data):
-			print m.group(0), 'not matched'
-		raise Exception
-	
-	#stop commented shell variables from looking like
-	#sun grid engine parameters
-	if sge == True: data = data.replace('#${','# ${')
-	
-	return data
+    #replace @{key} with value
+    for key in dic.iterkeys():
+        if not '@{%s}'%key in data: continue
+        data = data.replace('@{%s}'%key, str(dic[key]))
+
+    #check all placeholders were replaced
+    if '@{' in data:
+        for m in re.finditer('@{.*}',data):
+            print m.group(0), 'not matched'
+        raise Exception
+
+    #stop commented shell variables from looking like
+    #sun grid engine parameters
+    if sge == True: data = data.replace('#${','# ${')
+
+    return data
 
 def T(data,dic=None):
-	'''
-	fill out a template string
-	replaces @{key} with value
-	must replace all placeholders
-	not all keys need be present in the template
-	non string keys are ignored
-	'''
-	
-	if dic == None:
-		#get local variables from calling function
-		dic = get_locals(inspect.currentframe().f_back.f_locals)
-	
-	return sub_templates(data,dic)
+    '''
+    fill out a template string
+    replaces @{key} with value
+    must replace all placeholders
+    not all keys need be present in the template
+    non string keys are ignored
+    '''
+
+    if dic == None:
+        #get local variables from calling function
+        dic = get_locals(inspect.currentframe().f_back.f_locals)
+
+    return sub_templates(data,dic)
 
 def run(cmd,dic=None):
     '''
@@ -170,75 +216,75 @@ def run(cmd,dic=None):
     return subprocess.check_output(cmd,shell=True)
 
 def myqsub2(inp,out,dic=None,sge=True,bintype=None,arrayjobs=0,mem=1.0,rt=2,name='myjob',ops=None):
-	'''
-	like qsub but automatically generates the SGE boiler plate
-	
-	inp - template file
-	out - output file
-	dic - key:value dictionary
-	
-	replace @{key} with value for all placeholders in the input file
-	write to output file
-	run qsub
-	
-	if dic == None, get key value pairs from name space of calling function
-	
-	sge == True => fix commented shell variables that confuse sun grid engine
-	
-	automatically generate the SGE boiler plate options
-	bintype guessed from extension of inp file, or set manually eg '/usr/bin/perl'
-	arrayjobs = number of array jobs
-	mem is gigabytes
-	rt is run time in hours
-	ops is list of options, minus the leading #$
-	eg ['-pe smp @{threads}']
-	'''
+    '''
+    like qsub but automatically generates the SGE boiler plate
 
-	if dic == None:
-		#get local variables from calling function
-		dic = get_locals(inspect.currentframe().f_back.f_locals)
-		
-	sge_header = []
-		
-	if bintype == None:
-		#auto detect binary type
-		if inp.endswith('.py'):
-			sge_header.append('#$ -S /usr/bin/python')
-		elif inp.endswith('.sh'):
-			sge_header.append('#$ -S /bin/sh')
-		else:
-			raise Exception
-	else:
-		sge_header.append('#$ -S '+bintype)
-		
-	sge_header.append('#$ -N '+name)
-	sge_header.append('#$ -cwd')
-	sge_header.append('#$ -V')
-	
-	if arrayjobs == 0:
-		sge_header.append('#$ -o ../logs/$JOB_NAME.$JOB_ID.out')
-		sge_header.append('#$ -e ../logs/$JOB_NAME.$JOB_ID.err')
-	else:
-		sge_header.append('#$ -o ../logs/$JOB_NAME.$JOB_ID.$TASK_ID.out')
-		sge_header.append('#$ -e ../logs/$JOB_NAME.$JOB_ID.$TASK_ID.err')
-		sge_header.append('#$ -t 1:%d'%arrayjobs)
-	
-	sge_header.append('#$ -l h_vmem=%.1fG'%mem)
-	sge_header.append('#$ -l h_rt=%d:00:00'%rt)
-	if ops != None:
-		for x in ops:
-			sge_header.append('#$ '+x)
-	
-	data = '\n'.join(sge_header) + '\n\n'
+    inp - template file
+    out - output file
+    dic - key:value dictionary
 
-	f = open(inp)
-	data += f.read()
-	f.close()
-	
-	data = sub_templates(data,dic,sge)
-	
-	fout = open(out,'wb')
-	fout.write(data)
-	fout.close()
-	
-	os.system('qsub %s'%out)
+    replace @{key} with value for all placeholders in the input file
+    write to output file
+    run qsub
+
+    if dic == None, get key value pairs from name space of calling function
+
+    sge == True => fix commented shell variables that confuse sun grid engine
+
+    automatically generate the SGE boiler plate options
+    bintype guessed from extension of inp file, or set manually eg '/usr/bin/perl'
+    arrayjobs = number of array jobs
+    mem is gigabytes
+    rt is run time in hours
+    ops is list of options, minus the leading #$
+    eg ['-pe smp @{threads}']
+    '''
+
+    if dic == None:
+        #get local variables from calling function
+        dic = get_locals(inspect.currentframe().f_back.f_locals)
+        
+    sge_header = []
+        
+    if bintype == None:
+        #auto detect binary type
+        if inp.endswith('.py'):
+            sge_header.append('#$ -S /usr/bin/python')
+        elif inp.endswith('.sh'):
+            sge_header.append('#$ -S /bin/sh')
+        else:
+            raise Exception
+    else:
+        sge_header.append('#$ -S '+bintype)
+        
+    sge_header.append('#$ -N '+name)
+    sge_header.append('#$ -cwd')
+    sge_header.append('#$ -V')
+
+    if arrayjobs == 0:
+        sge_header.append('#$ -o ../logs/$JOB_NAME.$JOB_ID.out')
+        sge_header.append('#$ -e ../logs/$JOB_NAME.$JOB_ID.err')
+    else:
+        sge_header.append('#$ -o ../logs/$JOB_NAME.$JOB_ID.$TASK_ID.out')
+        sge_header.append('#$ -e ../logs/$JOB_NAME.$JOB_ID.$TASK_ID.err')
+        sge_header.append('#$ -t 1:%d'%arrayjobs)
+
+    sge_header.append('#$ -l h_vmem=%.1fG'%mem)
+    sge_header.append('#$ -l h_rt=%d:00:00'%rt)
+    if ops != None:
+        for x in ops:
+            sge_header.append('#$ '+x)
+
+    data = '\n'.join(sge_header) + '\n\n'
+
+    f = open(inp)
+    data += f.read()
+    f.close()
+
+    data = sub_templates(data,dic,sge)
+
+    fout = open(out,'wb')
+    fout.write(data)
+    fout.close()
+
+    os.system('qsub %s'%out)
